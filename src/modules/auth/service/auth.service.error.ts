5bb8572c9d12162614fea6f4bd4d9d54d82ec705
genebson <mauryavero@gmail.com>
2026-02-08 20:42:03 +0100

    Add Auth service errors

--- src/modules/auth/service/auth.service.error.ts
+++ src/modules/auth/service/auth.service.error.ts
@@ -1,13 +1,15 @@
 import { BadRequestException } from '@nestjs/common';
 
-export class InvalidCredentialsError extends BadRequestException {
-  constructor() {
-    super('The given credentials are invalid');
+export class UserInvalidCredentialsError extends BadRequestException {
+  constructor(message = 'The given credentials are invalid') {
+    super(message);
+    this.name = 'InvalidCredentialsError';
   }
 }
 
 export class UserAlreadyExistsError extends BadRequestException {
-  constructor() {
-    super('The given email is already in use');
+  constructor(message = 'The given email is already in use') {
+    super(message);
+    this.name = 'UserAlreadyExistsError';
   }
 }
