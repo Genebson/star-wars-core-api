722b4fe062f791a7f6d0a27b396a0cf17e59f39e
genebson <mauryavero@gmail.com>
2026-02-08 20:49:05 +0100

    Rename variables inside movie.repository.interface.ts

--- src/modules/movie/repository/interface/movie.repository.interface.ts
+++ src/modules/movie/repository/interface/movie.repository.interface.ts
@@ -5,8 +5,8 @@ import { IUpdateMovieInput } from './update-movie.input.interface';
 export interface IMovieRepository {
   findAll(): Promise<MovieEntity[]>;
   findById(id: number): Promise<MovieEntity>;
-  create(movieData: ICreateMovieInput): Promise<MovieEntity>;
-  updateById(id: number, movieData: IUpdateMovieInput): Promise<MovieEntity>;
+  create(movie: ICreateMovieInput): Promise<MovieEntity>;
+  updateById(id: number, movie: IUpdateMovieInput): Promise<MovieEntity>;
   deleteById(id: number): Promise<void>;
-  syncMovies(): Promise<MovieEntity[]>;
+  syncMovies(): Promise<void>;
 }
